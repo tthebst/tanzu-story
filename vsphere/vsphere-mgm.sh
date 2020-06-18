9f7955ccc0871ca826b2668446c9cb692a58d065
Tim Gretler <gretler.tim@gmail.com>
2020-06-18 16:41:29 +0200

    updated vpshere install script

--- vsphere/vsphere-mgm.sh
+++ vsphere/vsphere-mgm.sh
@@ -9,28 +9,32 @@ datastore=$6
 resource_pool=$7
 haproxy_image=$8
 
+whoami
+
 #create vm in vcenter from TKG ovas
-sudo -u tkg bash -c "govc folder.create -u $user:"$password"@$ip -k=true -dc=$datacenter /$datacenter/vm/TKG"
+govc folder.create -persist-session=false -u $user:"$password"@$ip -k=true -dc=$datacenter /$datacenter/vm/TKG
 
-sudo -u tkg bash -c "govc import.spec $photon_image.ova | jq ".Name=\"$photon_image\"" | jq '.NetworkMapping[0].Network="VM Network"' >$photon_image.json"
+govc import.spec /home/tkg/$photon_image.ova | jq ".Name=\"$photon_image\"" | jq '.NetworkMapping[0].Network="VM Network"' >/home/tkg/$photon_image.json
 
-sudo -u tkg bash -c "govc import.ova -u $user:"$password"@$ip -k=true -dc=$datacenter -ds=$datastore -pool=$resource_pool -options=$photon_image.json $HOME/$photon_image.ova"
+govc import.ova -persist-session=false -u $user:"$password"@$ip -k=true -dc=$datacenter -ds=$datastore -pool=$resource_pool -options=/home/tkg/$photon_image.json /home/tkg/$photon_image.ova
 
-sudo -u tkg bash -c "govc object.mv -u $user:"$password"@$ip -k=true -dc=$datacenter /$datacenter/vm/$photon_image /$datacenter/vm/TKG"
+govc object.mv -persist-session=false -u $user:"$password"@$ip -k=true -dc=$datacenter /$datacenter/vm/$photon_image /$datacenter/vm/TKG
 
-sudo -u tkg bash -c "govc import.spec $haproxy_image.ova | jq ".Name=\"$haproxy_image\"" | jq '.NetworkMapping[0].Network="VM Network"' >$haproxy_image.json"
+govc import.spec /home/tkg/$haproxy_image.ova | jq ".Name=\"$haproxy_image\"" | jq '.NetworkMapping[0].Network="VM Network"' >/home/tkg/$haproxy_image.json
 
-sudo -u tkg bash -c "govc import.ova -u $user:"$password"@$ip -k=true -dc=$datacenter -ds=$datastore -pool=$resource_pool -options=$haproxy_image.json $HOME/$haproxy_image.ova"
+govc import.ova -persist-session=false -u $user:"$password"@$ip -k=true -dc=$datacenter -ds=$datastore -pool=$resource_pool -options=/home/tkg/$haproxy_image.json /home/tkg/$haproxy_image.ova
 
-sudo -u tkg bash -c "govc object.mv -u $user:"$password"@$ip -k=true -dc=$datacenter /$datacenter/vm/$haproxy_image /$datacenter/vm/TKG"
+govc object.mv -persist-session=false -u $user:"$password"@$ip -k=true -dc=$datacenter /$datacenter/vm/$haproxy_image /$datacenter/vm/TKG
 
-sudo -u tkg bash -c "govc snapshot.create -u $user:"$password"@$ip -k=true -dc=$datacenter -vm /$datacenter/vm/TKG/$photon_image root"
+govc snapshot.create -persist-session=false -u $user:"$password"@$ip -k=true -dc=$datacenter -vm /$datacenter/vm/TKG/$photon_image root
 
-sudo -u tkg bash -c "govc vm.markastemplate -u $user:"$password"@$ip -k=true -dc=$datacenter /$datacenter/vm/TKG/$photon_image"
+govc vm.markastemplate -persist-session=false -u $user:"$password"@$ip -k=true -dc=$datacenter /$datacenter/vm/TKG/$photon_image
 
-sudo -u tkg bash -c "govc snapshot.create -u $user:"$password"@$ip -k=true -dc=$datacenter -vm /$datacenter/vm/TKG/$haproxy_image root"
+govc snapshot.create -persist-session=false -u $user:"$password"@$ip -k=true -dc=$datacenter -vm /$datacenter/vm/TKG/$haproxy_image root
 
-sudo -c "govc vm.markastemplate -u $user:"$password"@$ip -k=true -dc=$datacenter /$datacenter/vm/TKG/$haproxy_image"
+govc vm.markastemplate -persist-session=false -u $user:"$password"@$ip -k=true -dc=$datacenter /$datacenter/vm/TKG/$haproxy_image
 
 # turn off vsphere 7 kubernetes add on
 sshpass -p "$password" ssh -o StrictHostKeyChecking=no "$user"@$ip 'service-control --stop wcp'
+
+yes | tkg init --infrastructure=vsphere
